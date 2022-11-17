5a9f12d6254c96ed257fcb151c84a314ea61dc1d
itariss <itariss@protonmail.com>
2022-11-17 18:58:55 -0300

    Smooth scrolling and texts updating

--- js/animations.js
+++ js/animations.js
@@ -12,3 +12,13 @@ skillCards.forEach(item => {
 		console.log(item);
 	});
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth"
+		});
+	});
+});
